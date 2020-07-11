dafc1beea57ee613b243869dc0736d69b13af413
LucianoBWille <00lucianobwille00@gmail.com>
2020-07-10 23:36:38 -0300

    Criação da base do menu e da home

---
+++ src/components/Menu.vue
@@ -0,0 +1,56 @@
+<template>
+    <div class="ioMenu">
+        <router-link to="/">
+            <q-icon class="icon" name="home"/>
+        </router-link>
+        <br>
+        <router-link to="/about">
+            <!--<q-icon class="icon" name="build"/>
+            <q-icon class="icon" name="handyman"/>-->
+            <q-icon class="icon" name="construction"/>
+        </router-link>
+        <br>
+        <router-link to="/about">
+            <q-icon class="icon" name="school"/>
+        </router-link>
+        <br>
+        <router-link to="/about">
+            <q-icon class="icon" name="phone_enabled"/>
+        </router-link>
+        <br>
+        <router-link disabled to="/about">
+            <q-icon class="icon" name="message"/>
+            <!--<q-icon class="icon" name="edit"/>-->
+        </router-link>
+        <br>
+        <router-link to="/about">
+            <q-icon class="icon" name="group"/>
+        </router-link>
+        <br>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'IoMenu',
+        
+    }
+</script>
+
+<style scoped>
+.ioMenu{
+    border-radius: 2vw;
+    width: 4vw;
+    background-color: #ffffff;
+    position: fixed;
+    top: 25vh; 
+    left: 1vw;
+    z-index: 500;
+    text-align: center;
+}
+.icon {
+    font-size: 3vw;
+    color: black;
+    padding: 4px;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Input/Output logo" src="../assets/logo.png" width="15%" height="15%">
+    <p>
+      A Input Output é uma Empresa Júnior formada por estudantes na área de Ciências da Computação que buscam criar um ambiente empresarial e de trabalho, realizando serviços em software. 
+    </p>
+    <p>
+      Trazendo assim conhecimentos de volta aos seus integrantes e ao instituto de ensino que fazem parte. 
+    </p>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
-  }
 }
 </script>
